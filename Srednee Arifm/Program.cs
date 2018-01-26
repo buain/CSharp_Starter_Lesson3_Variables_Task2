23ece081ca0b552cb5a644dc1b06266ca3bfcef7
Vladimir <33662107+buain@users.noreply.github.com>
2018-01-26 17:50:45 +0300

    Initial commit

---
+++ Srednee Arifm/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Srednee_Arifm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 35;
+            int k = 25;
+            int h = 57;
+            int s = 0;
+            s = (x+k+h)/3;
+            Console.WriteLine(s);
+            Console.ReadLine();
+        }
+    }
+}
